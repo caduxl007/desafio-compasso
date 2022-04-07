326f85cd641b3e4f59dac044a699cd7a3c7e9c51
caduxl007 <eduardo2020dev@gmail.com>
2022-04-07 15:08:00 -0300

    feat: add component Button

---
+++ src/components/Button/index.tsx
@@ -0,0 +1,15 @@
+import { ButtonHTMLAttributes } from 'react';
+
+import './styles.scss';
+
+interface ButtonProps extends ButtonHTMLAttributes<HTMLButtonElement> {
+  isLoading?: boolean;
+}
+
+export function Button({ isLoading = false, children, ...rest }: ButtonProps) {
+  return (
+    <button {...rest} className="button">
+      {isLoading ? 'Carregando...' : children}
+    </button>
+  );
+}
