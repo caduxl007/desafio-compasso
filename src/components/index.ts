5007e3e4a7c71e907d70b92681d8bc723aa87f01
caduxl007 <eduardo2020dev@gmail.com>
2022-04-07 20:35:15 -0300

    feat: add page Header

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,1 +1,5 @@
 export { Header } from './Header';
+export { Title } from './Title';
+export { CardGithubUser } from './CardGithubUser';
+export { CardRepository } from './CardRepository';
+export { Button } from './Button';
--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,10 +1,12 @@
+import { Recents, Home, Profile } from 'pages';
 import { Routes, Route } from 'react-router-dom';
-import { Home } from '../pages/Home';
 
 export function RoutesApp() {
   return (
     <Routes>
       <Route path="/" element={<Home />} />
+      <Route path="/recents" element={<Recents />} />
+      <Route path="/profile/:username" element={<Profile />} />
     </Routes>
   );
 }
